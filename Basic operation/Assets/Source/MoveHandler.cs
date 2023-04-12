ddfd38c386570b28b28ea442ae0344189a567d83
jaeil777 <130571361+jaeil777@users.noreply.github.com>
2023-04-12 23:42:56 +0900

    1days

---
+++ Basic operation/Assets/Source/MoveHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+            Debug.Log("키 눌려졋어요");
+        if (Input.anyKey)
+            Debug.Log("키 누르고있어요");
+    }
+}
